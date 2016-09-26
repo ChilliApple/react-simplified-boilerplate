1a390a35ee086a6c22b26595ce8802e959ac88c7
Sandeep <sandeep@nethuesindia.com>
2016-09-26 15:59:50 +0530

    Init Boilerplate

---
+++ js/components/Comp1/index.js
@@ -0,0 +1,28 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import styles from './styles.css';
+
+// This is stateful component
+export default class Comp1 extends Component {
+
+  state: any = {
+    status: false,
+  };
+
+  changeState: any = () => {
+    const status = !this.state.status;
+    this.setState({
+      status,
+    });
+  };
+
+  render() {
+    return (
+      <div className={styles.root}>
+        <div>Comp-1, State: {this.state.status.toString()}</div>
+        <button onClick={this.changeState}>Change state</button>
+      </div>
+    );
+  }
+}
---
+++ js/components/index.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+import Comp1 from './Comp1';
+import Comp2 from './Comp2';
+import App from './App';
+
+module.exports = {
+  App,
+  Comp1,
+  Comp2,
+};
